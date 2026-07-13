543138b4d6236958aed08b13e91edcd7ed7e7e5c
red213black-creator <red213black@gmail.com>
2026-07-14 00:10:59 +0300

    Create Keybinds.java

---
+++ src/main/java/com/antishield/Keybinds.java
@@ -0,0 +1,35 @@
+package com.antishield;
+
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+
+import net.minecraft.client.option.KeyBinding;
+
+import org.lwjgl.glfw.GLFW;
+
+
+public class Keybinds {
+
+
+    public static KeyBinding activate;
+
+
+    public static void register() {
+
+
+        activate = KeyBindingHelper.registerKeyBinding(
+
+                new KeyBinding(
+
+                        "key.antishield.activate",
+
+                        GLFW.GLFW_KEY_K,
+
+                        "category.antishield"
+
+                )
+
+        );
+
+    }
+
+}
