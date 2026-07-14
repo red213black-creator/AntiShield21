92a6a4ccad8919d1e84b866428e86af915b0e9a5
red213black-creator <red213black@gmail.com>
2026-07-14 12:18:55 +0300

    Update AntiShieldClient.java

--- src/main/java/com/antishield/AntiShieldClient.java
+++ src/main/java/com/antishield/AntiShieldClient.java
@@ -7,7 +7,7 @@ public class AntiShieldClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         AntiShield.init();
-        ElytraFly.init()
+        ElytraFly.init();
         Keybinds.register();
     }
 
