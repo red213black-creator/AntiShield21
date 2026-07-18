7eda54eaaac5cf44f6f235349f972a6b9a215b92
red213black-creator <red213black@gmail.com>
2026-07-18 18:39:42 +0300

    Update AntiShieldClient.java

--- src/main/java/com/antishield/AntiShieldClient.java
+++ src/main/java/com/antishield/AntiShieldClient.java
@@ -3,14 +3,9 @@ package com.antishield;
 import net.fabricmc.api.ClientModInitializer;
 
 public class AntiShieldClient implements ClientModInitializer {
-
     @Override
     public void onInitializeClient() {
-        AntiShield.init();
         ElytraFly.init();
-        TntCart.init();
-        AutoCharge.init();
         Keybinds.register();
     }
-
 }
