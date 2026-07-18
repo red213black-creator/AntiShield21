b755370a3b159efaaef6dfd26d8d9de852f6b4a4
red213black-creator <red213black@gmail.com>
2026-07-18 20:22:02 +0300

    Update Keybinds.java

--- src/main/java/com/antishield/Keybinds.java
+++ src/main/java/com/antishield/Keybinds.java
@@ -2,6 +2,7 @@ package com.antishield;
 
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class Keybinds {
@@ -11,7 +12,7 @@ public class Keybinds {
         toggleFly = KeyBindingHelper.registerKeyBinding(
                 new KeyBinding(
                         "key.antishield.togglefly",
-                        KeyBinding.Type.KEYSYM,
+                        InputUtil.Type.KEYSYM,
                         GLFW.GLFW_KEY_V,
                         "category.antishield"
                 )
