afc0f973ff4b32712f7ef429c641b2c23a483ec6
red213black-creator <red213black@gmail.com>
2026-07-18 18:40:45 +0300

    Update Keybinds.java

--- src/main/java/com/antishield/Keybinds.java
+++ src/main/java/com/antishield/Keybinds.java
@@ -5,19 +5,15 @@ import net.minecraft.client.option.KeyBinding;
 import org.lwjgl.glfw.GLFW;
 
 public class Keybinds {
-
-    public static KeyBinding activate;
+    public static KeyBinding toggleFly;
 
     public static void register() {
-
-        activate = KeyBindingHelper.registerKeyBinding(
+        toggleFly = KeyBindingHelper.registerKeyBinding(
                 new KeyBinding(
-                        "key.antishield.activate",
-                        GLFW.GLFW_KEY_K,
+                        "key.antishield.togglefly",
+                        GLFW.GLFW_KEY_V,
                         "category.antishield"
                 )
         );
-
     }
-
 }
